512b396a49080135eb86473d41011257bd323519
Joachim Mero <jmero@netflix.com>
2026-02-03 14:43:06 +0200

    Revert "Migrate from omnisharp to roslyn"
    This reverts commit 54572a87d377bffa2bb31c646d9fa47cf59232a6.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,7 +3,13 @@ return {
   opts = {
     servers = {
       omnisharp = {
-        enabled = false
+        settings = {
+          RoslynExtensionsOptions = {
+            -- Set to false to analyze the whole solution instead of just open files
+            AnalyzeOpenDocumentsOnly = false,
+            EnableAnalyzersSupport = true,
+          },
+        },
       },
     },
   },
